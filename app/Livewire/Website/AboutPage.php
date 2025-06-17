83549075af471e14351e4a97424c70ce95c1c013
dkAsCambodia <nitindamo50@gmail.com>
2025-06-17 14:34:48 +0700

    setup projects header and footer nabvar

---
+++ app/Livewire/Website/AboutPage.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Livewire\Website;
+use Livewire\Component;
+
+class AboutPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.website.about-page');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Website\HomePage;
+use App\Livewire\Website\AboutPage;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 Route::get('/', HomePage::class)->name('home');
+Route::get('/about-us/company-overview', AboutPage::class)->name('aboutus');
